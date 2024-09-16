d998ab0cf5bb236b533daf3097f3cc4a8594de5a
Yanga Rubushe <zilithemba10@gmail.com>
2024-09-16 04:27:17 +0200

    "Added file upload functionality to CourseBasicInfo component"

--- app/create-course/[courseId]/_components/CourseBasicInfo.jsx
+++ app/create-course/[courseId]/_components/CourseBasicInfo.jsx
@@ -1,11 +1,19 @@
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import { Puzzle } from 'lucide-react';
 import { Button } from '../../../../components/ui/button';
 import EditCourseBasicInfo from './EditCourseBasicInfo';
 
 
 function CourseBasicInfo({course,refreshData}) {
+
+  const [selectedFile,setSelectedFile]=useState();
+  const onFileSelected = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(URL.createObjectURL(file));
+  }
+
+
   return (
     <div className='p-10 border rounded-xl shadow-sm mt-5'>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-5 items-center'>
@@ -16,14 +24,18 @@ function CourseBasicInfo({course,refreshData}) {
           <h2 className='font-medium mt-2 flex gap-2 items-center text-primary'><Puzzle />{course?.category}</h2>
           <Button className='w-full mt-5'>Start</Button>
         </div>
-        <div className='flex justify-center items-center'>
+        <div >
+          <label htmlFor='upload-image' className='flex justify-center items-center'>
           <Image 
-            src={'/2702154.png'} 
+            src={selectedFile?selectedFile: '/2702154.png'} 
             width={200} 
             height={200}
             alt="Course Image"
-            className='rounded-xl h-[200px]'
+            className='rounded-xl h-[200px] cursor-pointer'
           />
+          </label>
+          <input type='file' id="upload-image" className='opacity-0' onChange={onFileSelected}/>
+          
         </div>
       </div>
     </div>
